8f168c6b002fabf7aed97a441f348c4243ee1c2b
Lostzoro9805 <156735628+Lostzoro9805@users.noreply.github.com>
2024-01-20 17:01:19 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -19,19 +19,16 @@ from decouple import config
 
 
 try:
-    APP_ID = config("APP_ID", default=6, cast=int)
+    APP_ID = config("APP_ID", "4018758")
     # APP_ID = ""
-    API_HASH = config("API_HASH", default="eb06d4abfb49dc3eeb1aeb98ae0f581e")
+    API_HASH = config("API_HASH", "622bba3cf046315531f71f9d97fa6c2a")
     # API_HASH = ""
-    BOT_TOKEN = config("BOT_TOKEN")
+    BOT_TOKEN = config("BOT_TOKEN", "6942815361:AAHwpmE7ThoaKWJJyRIrIZHRXOaxooCXGd8")
     # BOT_TOKEN = ""
-    DEV = 1322549723
-    OWNER = config("OWNER")
+    DEV = 5385471287
+    OWNER = config("OWNER", "5385471287")
     # OWNER = ""
-    FFMPEG = config(
-        "FFMPEG",
-        default='ffmpeg -i "{}" -preset ultrafast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{}"',
-    )
+    FFMPEG = config("FFMPEG", "ffmpeg -i "{}" -preset veryfast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy "{}"
     # FFMPEG = ""
     THUMB = config(
         "THUMBNAIL", default="https://telegra.ph/file/ab23f5209aae9cae3ba3c.jpg"
