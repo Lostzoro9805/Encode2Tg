76a69a01f22c881a2a4a7428cc96153f0ea6f99b
GokuOp7 <153523489+GokuOp7@users.noreply.github.com>
2024-01-23 10:54:12 +0530

    Update worker.py

--- bot/worker.py
+++ bot/worker.py
@@ -691,8 +691,17 @@ async def pencode(message):
         except Exception:
             pass
         nnn = await xxx.edit("`▲ Uploading ▲`")
-        fname = out.split("/")[1]
-        ds = await upload2(app, message.from_user.id, out, nnn, thum)
+        ok = await upload_file(
+                client=e.client,
+                file=f,
+                name=out,
+                progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                    progress(d, t, nnn, ttt, "Uploadin'")
+                ),
+            )
+        ds = await e.client.send_file(
+            e.chat_id, file=ok, force_document=True, thumb=thum
+        )
         await nnn.delete()
         if LOG_CHANNEL:
             chat = int(LOG_CHANNEL)
