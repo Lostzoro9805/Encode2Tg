2130824ee78de3494a2ca7995bc165df5ad0c658
Lostzoro9805 <156735628+Lostzoro9805@users.noreply.github.com>
2025-05-21 18:13:28 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -23,14 +23,14 @@ try:
     # APP_ID = ""
     API_HASH = config("API_HASH", "622bba3cf046315531f71f9d97fa6c2a")
     # API_HASH = ""
-    BOT_TOKEN = config("BOT_TOKEN", "6942815361:AAHwpmE7ThoaKWJJyRIrIZHRXOaxooCXGd8")
+    BOT_TOKEN = config("BOT_TOKEN", "6778992913:AAEJW5hhggt8-V8AKxUE97nr859wJIl-U6c")
     # BOT_TOKEN = ""
     DEV = 5385471287
-    OWNER = config("OWNER", "5822396703")
+    OWNER = config("OWNER", "5385471287")
     # OWNER = ""
     FFMPEG = config("FFMPEG", "ffmpeg -i '''{}''' -c:v libx265 -crf 28 -c:s copy? -s 854x480 -preset veryfast -ab 35k '''{}''' -y")
     #FFMPEG = 
-    THUMB = config("THUMBNAIL", "https://telegra.ph/file/ab23f5209aae9cae3ba3c.jpg")
+    THUMB = config("THUMBNAIL", "https://telegra.ph/file/ab23f5209aae9cae3a3c.jpg")
     # THUMB = ""
     ICON = config("ICON", "https://te.legra.ph/file/462b5a002f80bdf8a1ec1.png")
     # ICON = ""
